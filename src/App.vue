363ddfad0875d43d268ca028850f891fda5a9537
hoanganleba <hoanganleba@gmail.com>
2025-06-10 17:03:13 +0700

    refactor App.vue to improve component imports and enhance drag-and-drop functionality; update DragAndDropView styles for better visibility

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
-import Titlebar from './components/TitleBar.vue'
-import { defineAsyncComponent, onMounted, ref } from 'vue'
+import { defineAsyncComponent, nextTick, onMounted, ref } from 'vue'
 import { open } from '@tauri-apps/plugin-dialog'
-import DragAndDropView from './components/DragAndDropView.vue'
-import Overlay from './components/Overlay.vue'
 import { listen } from '@tauri-apps/api/event'
 
 const ImageView = defineAsyncComponent(
     () => import('./components/ImageView.vue')
+)
+
+const DragAndDropView = defineAsyncComponent(() =>
+    import('./components/DragAndDropView.vue')
+)
+
+const Overlay = defineAsyncComponent(() =>
+    import('./components/Overlay.vue')
+)
+
+const Titlebar = defineAsyncComponent(() =>
+    import('./components/TitleBar.vue')
 )
 
 const folderPath = ref('')
@@ -24,7 +33,7 @@ function openFolder() {
     })
 }
 
-onMounted(async () => {
+onMounted(() => {
     window.addEventListener('keydown', (e) => {
         if ((e.metaKey || e.ctrlKey) && e.key.toLowerCase() === 'o') {
             e.preventDefault()
@@ -36,19 +45,21 @@ onMounted(async () => {
             e.preventDefault()
         })
     }
-    await listen('tauri://drag-over', () => {
-        isOverlay.value = true
-    })
-    await listen('tauri://drag-leave', () => {
-        isOverlay.value = false
-    })
-    await listen<{ paths: string[] }>('tauri://drag-drop', (event) => {
-        const droppedPath = event.payload.paths[0]
-        // Check if droppedPath looks like a directory (no file extension)
-        if (droppedPath && !/\.[^\/\\]+$/.test(droppedPath)) {
-            folderPath.value = droppedPath
-        }
-        isOverlay.value = false
+    nextTick(() => {
+        listen('tauri://drag-over', () => {
+            isOverlay.value = true
+        })
+        listen('tauri://drag-leave', () => {
+            isOverlay.value = false
+        })
+        listen<{ paths: string[] }>('tauri://drag-drop', (event) => {
+            const droppedPath = event.payload.paths[0]
+            // Check if droppedPath looks like a directory (no file extension)
+            if (droppedPath && !/\.[^\/\\]+$/.test(droppedPath)) {
+                folderPath.value = droppedPath
+            }
+            isOverlay.value = false
+        })
     })
 })
 </script>
@@ -62,19 +73,11 @@ onMounted(async () => {
             leave-from-class="opacity-100 scale-100 blur-none" leave-to-class="opacity-0 scale-95 blur-sm">
             <Overlay v-if="isOverlay" />
         </transition>
-        <transition enter-active-class="transition duration-300 ease-out"
-            enter-from-class="opacity-0 blur-sm scale-95" enter-to-class="opacity-100 blur-0 scale-100"
-            leave-active-class="transition duration-300 ease-in" leave-from-class="opacity-100 blur-0 scale-100"
-            leave-to-class="opacity-0 blur-sm scale-95" mode="out-in">
+        <transition enter-active-class="transition duration-300 ease-out" enter-from-class="opacity-0 blur-sm scale-95"
+            enter-to-class="opacity-100 blur-0 scale-100" leave-active-class="transition duration-300 ease-in"
+            leave-from-class="opacity-100 blur-0 scale-100" leave-to-class="opacity-0 blur-sm scale-95" mode="out-in">
             <DragAndDropView v-if="!folderPath" :openFolderFnProps="openFolder" />
             <ImageView v-else :folderPath="folderPath" :key="folderPath" />
         </transition>
     </div>
 </template>
-
-<style>
-html,
-body {
-    @apply select-none overscroll-none overflow-hidden;
-}
-</style>
